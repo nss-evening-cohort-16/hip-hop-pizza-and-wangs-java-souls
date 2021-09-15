0d3fa1ca8e2731ed1cea5626693565ea99d26ef9
Derek Baylor <dbaylor25@gmail.com>
2021-09-15 18:48:01 -0500

    merging

--- src/scripts/helpers/data/homeScreenButtonsCard.js
+++ src/scripts/helpers/data/homeScreenButtonsCard.js
@@ -4,11 +4,11 @@ const showHSbuttons = () => {
   clearDom();
 
   document.querySelector('#button-container').innerHTML += `
-      <div class="card">
-          <div>
-              <div id="view-orders-button" class=" btn hs-btn view-orders-btn">View Orders</div>
-              <div id="create-order-button" class="btn hs-btn create-order-btn">Create an Order</div>
-              <div id="view-revenue-button" class="btn hs-btn view-revenue-btn">View Revenue</div>
+      <div class="card button-card">
+          <div class="btn-card-body">
+              <div id="view-orders-button" class=" btn btn-lg btn-success hs-btn view-orders-btn">View Orders</div>
+              <div id="create-order-button" class="btn btn-lg btn-warning hs-btn create-order-btn">Create an Order</div>
+              <div id="view-revenue-button" class="btn btn-lg btn-info hs-btn view-revenue-btn">View Revenue</div>
           </div>
       </div>
       `;
