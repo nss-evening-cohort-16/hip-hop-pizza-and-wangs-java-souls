b496da318375746a5c046fdb26f5a6c253ffb0fa
Klay Thacker <klaythacker11@gmail.com>
2021-09-15 17:32:48 -0500

    commit before merge

--- src/scripts/views/startApp.js
+++ src/scripts/views/startApp.js
@@ -1,9 +1,11 @@
 import domBuilder from '../components/domBuilder';
 import navBar from '../components/navBar';
+import showOrders from '../components/orders';
 
 const startApp = () => {
   domBuilder();
   navBar();
+  showOrders();
 };
 
 export default startApp;
