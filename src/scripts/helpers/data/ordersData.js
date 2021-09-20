ccd9c85597d437d5da52ceefcb57220963b8be5b
Grace Sutherland <rgs1690@gmail.com>
2021-09-20 18:46:46 -0500

    m

--- src/scripts/helpers/data/ordersData.js
+++ src/scripts/helpers/data/ordersData.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import firebaseConfig from '../../../api/apiKeys';
-import orderItemForm from '../../components/forms/OrderItemForm';
+import orderItemForm from '../../components/forms/orderItemForm';
 
 const dbUrl = firebaseConfig.databaseURL;
 // get orders
