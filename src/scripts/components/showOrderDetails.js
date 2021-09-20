4f0d3959477dbd47cf361d53601af33b08843a66
Klay Thacker <klaythacker11@gmail.com>
2021-09-20 14:53:41 -0500

    edit comment

--- src/scripts/components/showOrderDetails.js
+++ src/scripts/components/showOrderDetails.js
@@ -1,26 +1,26 @@
-import clearDom from '../helpers/auth/clearDom';
+// import clearDom from '../helpers/auth/clearDom';
 
-const showOrderDetails = (array) => {
-  clearDom();
+// const showOrderDetails = (array) => {
+//   clearDom();
 
-  array.forEach((item) => {
-    document.querySelector('#shop').innerHTML += `
-    <div class="card" style="width: 18rem;">
-        <div class="card-body">
-        <h5 class="card-title">${item.customerName}
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">${item.menuitem}</li>
-          <li class="list-group-item">${item.itemprice}</li>
-        </ul>
-        <div class="card-body">
-          <button type="button" class="btn btn-outline-info">Edit Item</button>
-          <button type="button" class="btn btn-outline-danger">Delete Item</button>
-        </div>
-    </div>`;
-  });
+//   array.forEach((item) => {
+//     document.querySelector('#shop').innerHTML += `
+//     <div class="card" style="width: 18rem;">
+//         <div class="card-body">
+//         <h5 class="card-title">${item.customerName}
+//         </div>
+//         <ul class="list-group list-group-flush">
+//           <li class="list-group-item">${item.menuitem}</li>
+//           <li class="list-group-item">${item.itemprice}</li>
+//         </ul>
+//         <div class="card-body">
+//           <button type="button" class="btn btn-outline-info">Edit Item</button>
+//           <button type="button" class="btn btn-outline-danger">Delete Item</button>
+//         </div>
+//     </div>`;
+//   });
 
-  document.querySelector('#button-container').innerHTML = '<button type="button" class="btn btn-outline-success btn-lg" id="add-items-btn">Add Items</button><button type="button" class="btn btn-outline-primary btn-lg" id="payment-btn">Go To Payment</button>';
-};
+//   document.querySelector('#button-container').innerHTML = '<button type="button" class="btn btn-outline-success btn-lg" id="add-items-btn">Add Items</button><button type="button" class="btn btn-outline-primary btn-lg" id="payment-btn">Go To Payment</button>';
+// };
 
-export default showOrderDetails;
+// export default showOrderDetails;
