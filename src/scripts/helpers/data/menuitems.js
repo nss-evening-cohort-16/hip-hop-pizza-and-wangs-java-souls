33d2068a723dc526e2ed7ce1cf31972d64f6dc3c
RGKBH <rubiblr@gmail.com>
2021-09-20 18:14:07 -0500

    upto payment method and tip

--- src/scripts/helpers/data/menuitems.js
+++ src/scripts/helpers/data/menuitems.js
@@ -7,17 +7,10 @@ const dbUrl = firebaseConfig.databaseURL;
 //     .then((response) => resolve(Object.values(response.data)))
 //     .catch((error) => reject(error));
 // });
-// const getMenuItemsArray = (MenuItemsArray) => new Promise((resolve, reject) => {
-//   let ordertotal = 0;
-//   MenuItemsArray.forEach((item) => {
-//   axios.get(`${dbUrl}/menuItem.json?orderBy="firebaseKey"&equalTo="${item.menuItemID}"`)
-//     .then((response) => resolve(response.data.itemPrice))
-//     .catch(reject);
-// });
+
 const getMenuItemsArray = (item) => new Promise((resolve, reject) => {
   axios.get(`${dbUrl}/menuItem.json?orderBy="firebaseKey"&equalTo="${item.menuItemID}"`)
     .then((response) => resolve(response.data.itemPrice))
     .catch(reject);
 });
-
 export default getMenuItemsArray;
