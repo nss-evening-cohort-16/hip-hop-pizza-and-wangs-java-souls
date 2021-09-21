9ec0e3f35678f0a092cc721a93038c07e8ba35ab
Grace Sutherland <rgs1690@gmail.com>
2021-09-21 15:37:37 -0500

    deleted comment

--- src/scripts/helpers/data/revenueData.js
+++ src/scripts/helpers/data/revenueData.js
@@ -64,14 +64,6 @@ const getTotalGiftCardOrders = () => {
       document.querySelector('#giftCardOrders').innerHTML += `${giftCardOrders.length}`;
     });
 };
-// const formatDates = (date) => {
-//   const year = date.getFullYear();
-//   let month = (1 + date.getMonth()).toString();
-//   month = month.length > 1 ? month : 0 + month;
-//   let day = date.getDate().toString();
-//   day = day.length > 1 ? day : 0 + day;
-//   return `${month}/${day}/${year}`;
-// };
 const getRangeofDates = () => {
   getOrders()
     .then((ordersArray) => {
