86a9ca1eae7706dbb32f5cf13770a9ac22a14b0b
Derek Baylor <dbaylor25@gmail.com>
2021-09-20 19:02:36 -0500

    merging

--- src/scripts/events/domEvents.js
+++ src/scripts/events/domEvents.js
@@ -1,17 +1,10 @@
 import showOrders from '../components/orders';
-import {
-  getOrders,
-  createOrder,
-  deleteOrder,
-} from '../helpers/data/ordersData';
-import { createOrderitem } from '../helpers/data/orderItemsData';
-import paymentForm from '../components/forms/paymentForm';
-// import { createOrderitem, getOrderDetails } from '../helpers/data/orderItemsData';
-// import showOrderDetails from '../components/showOrderDetails';
+import { getOrders, createOrder, deleteOrder } from '../helpers/data/ordersData';
+import orderItemForm from '../components/forms/orderItemForm';
+import { createOrderitem, getOrderDetails } from '../helpers/data/orderItemsData';
+import showOrderDetails from '../components/showOrderDetails';
 import addOrderForm from '../components/forms/orderForm';
 import viewRevenuePage from '../components/revenue';
-import viewOrderItems from '../helpers/data/merdedData';
-import showOrderItems from '../components/showOrderItems';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -38,31 +31,24 @@ const domEvents = () => {
         timeStamp: new Date(),
         tipTotal: 0
       };
-
-      createOrder(orderObject);
+      createOrder(orderObject).then(orderItemForm(orderObject));
     }
 
-    if (e.target.id.includes('update-menuItem')) {
+    if (e.target.id.includes('submit-MenuItem')) {
       e.preventDefault();
-      console.warn(document.querySelector('#OrderItem_id').value);
-      const [order, menyItem] = document.querySelector('#OrderItem_id').value.split('--');
-      // console.warn(document.querySelector('#orderNum').firebasekey);
-      // const [menu, price] = document.querySelector('#OrderItem_id').selectedOptions[0].text.split('--');
+      console.warn('submit-MenuItem');
+      console.warn(document.querySelector('#OrderItem_id').selectedOptions[0].text);
+      console.warn(document.querySelector('#OrderItem_id').selectedOptions[0].value);
+      const [menu, price] = document.querySelector('#OrderItem_id').selectedOptions[0].text.split('--');
       const orderObject = {
-        orderID: order,
-        menuItemID: menyItem
+        menuitem: menu,
+        itemprice: price,
+        orderId: document.querySelector('#OrderItem_id').selectedOptions[0].value,
+        menuItemID: document.querySelector('#OrderItem_id').selectedOptions[0].value
       };
-      createOrderitem(orderObject);
-      // createOrderitem(orderObject).then(orderItemForm(orderObject));
-      // getOrderDetails().then((orderCards) => showOrderDetails(orderCards));
+      createOrderitem(orderObject).then(orderItemForm(orderObject));
+      getOrderDetails().then((orderCards) => showOrderDetails(orderCards));
     }
-
-    if (e.target.id.includes('payment')) {
-      e.preventDefault();
-      paymentForm();
-      console.warn('payment');
-    }
-
     // Delete Orders
     if (e.target.id.includes('delete-order')) {
       // eslint-disable-next-line no-alert
@@ -70,11 +56,6 @@ const domEvents = () => {
         const [, id] = e.target.id.split('--');
         deleteOrder(id).then(showOrders);
       }
-    }
-    // VIEW ORDERS
-    if (e.target.id.includes('details-btn')) {
-      const [, firebaseKey] = e.target.id.split('--');
-      viewOrderItems(firebaseKey).then(showOrderItems);
     }
   });
 };
